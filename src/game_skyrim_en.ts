acd71ea72dfd0be67f3b411a28b6a0c1eb750a1e
Tannin <sherb@gmx.net>
2016-06-04 20:34:30 +0200

    translation

---
+++ src/game_skyrim_en.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>GameSkyrim</name>
+    <message>
+        <location filename="gameskyrim.cpp" line="79"/>
+        <source>Adds support for the game Skyrim</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
